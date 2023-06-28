c8e8af40a50d16ca4004a2d84d43a8058f87d1bd
JakeSchroder <jake.schroder@gmail.com>
2023-06-28 15:24:37 -0500

    Init buttom filter and project card

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,5 +1,11 @@
 import clientPromise from "../../lib/mongodb";
 
+const typeFilter = ['Shop_All', 'T-Shirts', 'Tops', 
+                    'Layers', 'Pullovers', 'Shorts', 
+                    'Pants', 'Dresses_&_Skirts', 
+                    'Shoes', 'Jewelry', 'Accessories', 
+                    'Wildcard_Clothing', 'Goods'];
+
 export default async (req, res) => {
     try {
         const client = await clientPromise;
---
+++ pages/product_grid/product_card.js
@@ -0,0 +1,12 @@
+import Image from "next/image"
+
+export default function ProductCard(){
+    const image_link = 'https://cdn.shopify.com/s/files/1/0105/8592/products/IMG_8978.jpg?v=1675111387';
+    const description = 'Ring from tunnelvision'
+
+    return(
+        <div className=" ">
+            <Image alt={description} src={image_link} width={250} height={150} ></Image>
+        </div>
+    )
+}
