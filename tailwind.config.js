c9b6c88ef88e17ba1f68c44329d2ffbb35b34f02
JakeSchroder <jake.schroder@gmail.com>
2023-07-03 03:13:43 -0500

    Add montserrat alternative as a font

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ['var(--font-montserrat-alt)'],
+
+      }
     },
   },
   plugins: [],
