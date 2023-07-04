46847c1c8d39ce77a3f54fa9323b2bc516f90b72
JakeSchroder <jake.schroder@gmail.com>
2023-07-03 21:26:46 -0500

    Create default props for builds

--- pages/product_grid/product_card.js
+++ pages/product_grid/product_card.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 export default function ProductCard(props, index) {
   const errorImage = 'https://static2.bigstockphoto.com/0/8/2/large1500/2802007.jpg';
   const productURL = `${props.url}/products/${props.handle}?variant=${props.variants[0]['id']}`;
+  console.log(props)
 
   return (
     <div key={index} className="max-w-[250px] min-w-[250px]">
@@ -29,4 +30,25 @@ export default function ProductCard(props, index) {
       <h2 className=" text-center">{props.title}</h2>
     </div>
   );
+}
+ProductCard.defaultProps = {
+  title: '',
+  handle: '', 
+  variants: {
+    0:{
+      id:''
+    }
+  }, 
+  images: {
+    0:{
+      src:''
+    },
+    1:{
+      src:''
+    }
+  },
+  vendor: '',
+  tags: '',
+  product_type: '', 
+  url: '',
 }
