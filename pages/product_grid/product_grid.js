cccc62e44573d08dbdfa670d2d2f23fc225620a6
JakeSchroder <jake.schroder@gmail.com>
2023-07-06 19:53:36 -0500

    Add pagination to product grid

--- pages/product_grid/product_grid.js
+++ pages/product_grid/product_grid.js
@@ -1,16 +1,24 @@
 import ProductCard from "./product_card";
-import useSWR from 'swr';
+import useSWRInfinite from "swr/infinite";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const getKey = (pageIndex, previousPageData) => {
+  if (previousPageData && !previousPageData.length) return null; // reached the end
+  return `/api/products?orderReq=random&categoryReq=Shop_All&pageIndex=${pageIndex}&pageSize=12`; // SWR key
+};
 
-export default function ProductGrid(){
-    const { data, error, isLoading } = useSWR('/api/products?orderReq=random&categoryReq=Shop_All&pageIndex=5&pageSize=10', fetcher)
+export default function ProductGrid() {
+  const { data, size, setSize } = useSWRInfinite(getKey, fetcher);
+  if (!data) return "loading";
 
-    if (error) return <div>failed to load</div>
-    if (isLoading) return <div>loading...</div>
-    return(
-        <div className="  grid grid-cols-4 space-x-1  space-y-5 align-bottom ">
-            {data.map((product, index) => ProductCard({ ...product }, index))}
-        </div>
-    )
+  return (
+    <div className="  grid grid-cols-4 space-x-1  space-y-5 align-bottom ">
+      {data.map((products) => {
+        return products.map((product, index) =>
+          ProductCard({ ...product }, index * size)
+        );
+      })}
+      <button onClick={() => setSize(size + 1)}>Load More</button>
+    </div>
+  );
 }
